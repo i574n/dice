3fbdc5ba461deb62b8a54ff1d07c8188c6f48335
프리마 <fc1943s@pm.me>
2024-02-12 17:42:33 -0400

    tests

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -6,13 +6,13 @@ $ErrorActionPreference = "Stop"
 . ../../polyglot/scripts/core.ps1
 
 
-{ . "$ScriptDir/../../polyglot/apps/builder/build.ps1" } | Invoke-Block
-{ . "$ScriptDir/../../polyglot/apps/parser/build.ps1" } | Invoke-Block
-{ . "$ScriptDir/../../polyglot/lib/fsharp/build.ps1" } | Invoke-Block
-{ . "$ScriptDir/../../polyglot/apps/spiral/build.ps1" } | Invoke-Block
-{ . "$ScriptDir/../../polyglot/lib/spiral/build.ps1" } | Invoke-Block
-{ . "$ScriptDir/../../polyglot/apps/spiral/temp/extension/build.ps1" } | Invoke-Block
-{ . "$ScriptDir/../../polyglot/apps/dir-tree-html/build.ps1" } | Invoke-Block
+{ . "$ScriptDir/../../polyglot/apps/builder/build.ps1" -fast 1 } | Invoke-Block
+{ . "$ScriptDir/../../polyglot/apps/parser/build.ps1" -fast 1 } | Invoke-Block
+{ . "$ScriptDir/../../polyglot/lib/fsharp/build.ps1" -fast 1 } | Invoke-Block
+{ . "$ScriptDir/../../polyglot/apps/spiral/build.ps1" -fast 1 } | Invoke-Block
+{ . "$ScriptDir/../../polyglot/lib/spiral/build.ps1" -fast 1 } | Invoke-Block
+{ . "$ScriptDir/../../polyglot/apps/spiral/temp/extension/build.ps1" -fast 1 } | Invoke-Block
+{ . "$ScriptDir/../../polyglot/apps/dir-tree-html/build.ps1" -fast 1 } | Invoke-Block
 
 { . "$ScriptDir/../build.ps1" } | Invoke-Block
 { . "$ScriptDir/../fsharp/build.ps1" } | Invoke-Block
