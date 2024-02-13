abd3a77e81295e4addb22c1bb307386f8fd9fb6d
프리마 <fc1943s@pm.me>
2024-02-12 21:19:44 -0400

    tests

--- contract/build.ps1
+++ contract/build.ps1
@@ -9,8 +9,8 @@ $ErrorActionPreference = "Stop"
 
 { cargo build --release --target wasm32-unknown-unknown --features wasm } | Invoke-Block
 New-Item dist -ItemType Directory -Force | Out-Null
-Copy-Item ../../polyglot/target/wasm32-unknown-unknown/release/dice_contract.wasm dist/dice.wasm -Force
+Copy-Item ../target/wasm32-unknown-unknown/release/dice_contract.wasm dist/dice.wasm -Force
 
 if (!$fast) {
-    { ../../polyglot/target/release/dice_contract_tests } | Invoke-Block -Linux -EnvironmentVariables @{ "NEAR_RPC_TIMEOUT_SECS" = 100 }
+    { ../target/release/dice_contract_tests } | Invoke-Block -Linux -EnvironmentVariables @{ "NEAR_RPC_TIMEOUT_SECS" = 100 }
 }
