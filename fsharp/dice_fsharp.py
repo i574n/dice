f97142d02e2e57e4a86199de0fcdb53089224aee
프리마 <fc1943s@pm.me>
2024-03-29 14:03:38 -0400

    tests

--- fsharp/dice_fsharp.py
+++ fsharp/dice_fsharp.py
@@ -63,11 +63,11 @@ def roll_within_bounds(log: Callable[[str], None] | None, max: int, rolls: FShar
     match_value: tuple[int, FSharpList[int]] | None = accumulate_dice_rolls(log, rolls, length(rolls) - 1, 0)
     (pattern_matching_result, result_1) = (None, None)
     if match_value is not None:
-        def _arrow2(__unit: None=None, log: Any=log, max: Any=max, rolls: Any=rolls) -> bool:
+        def _arrow19(__unit: None=None, log: Any=log, max: Any=max, rolls: Any=rolls) -> bool:
             result: int = match_value[0] or 0
             return (result <= max) if (result >= 1) else False
 
-        if _arrow2():
+        if _arrow19():
             pattern_matching_result = 0
             result_1 = match_value[0]
 
@@ -105,7 +105,7 @@ def rotate_numbers(max: int, items: IEnumerable_1[int]) -> IEnumerable_1[int]:
 
 def create_sequential_roller(list_1: FSharpList[__A]) -> Callable[[], __A]:
     current_index: int = 0
-    def _arrow3(__unit: None=None, list_1: Any=list_1) -> __A:
+    def _arrow20(__unit: None=None, list_1: Any=list_1) -> __A:
         nonlocal current_index
         match_value: __A | None = try_item(current_index, list_1)
         if match_value is None:
@@ -117,11 +117,11 @@ def create_sequential_roller(list_1: FSharpList[__A]) -> Callable[[], __A]:
             return item
 
 
-    return _arrow3
+    return _arrow20
 
 
 def roll_progressively(log: Callable[[str], None] | None, roll: Callable[[], int], reroll: bool, max: int) -> int:
-    def _arrow4(__unit: None=None, log: Any=log, roll: Any=roll, reroll: Any=reroll, max: Any=max) -> int:
+    def _arrow21(__unit: None=None, log: Any=log, roll: Any=roll, reroll: Any=reroll, max: Any=max) -> int:
         max_1: int = max or 0
         def loop(n_mut: int, p_mut: int) -> int:
             while True:
@@ -142,7 +142,7 @@ def roll_progressively(log: Callable[[str], None] | None, roll: Callable[[], int
 
         return 1 if (max_1 == 1) else loop(0, 1)
 
-    power: int = (_arrow4() - 1) or 0
+    power: int = (_arrow21() - 1) or 0
     def loop_1(rolls_mut: FSharpList[int], size_mut: int, log: Any=log, roll: Any=roll, reroll: Any=reroll, max: Any=max) -> int:
         while True:
             (rolls, size) = (rolls_mut, size_mut)
@@ -176,10 +176,10 @@ def roll_progressively(log: Callable[[str], None] | None, roll: Callable[[], int
                     return result_1
 
                 elif pattern_matching_result == 1:
-                    def _arrow5(_arg: int, rolls: Any=rolls, size: Any=size) -> int:
+                    def _arrow22(_arg: int, rolls: Any=rolls, size: Any=size) -> int:
                         return roll(None)
 
-                    rolls_mut = initialize(power, _arrow5)
+                    rolls_mut = initialize(power, _arrow22)
                     size_mut = power
                     continue
 
@@ -195,24 +195,24 @@ def roll_progressively(log: Callable[[str], None] | None, roll: Callable[[], int
 
 
 def main(args: Array[str]) -> int:
-    def _arrow7(__unit: None=None, args: Any=args) -> Callable[[str], None]:
+    def _arrow24(__unit: None=None, args: Any=args) -> Callable[[str], None]:
         clo: Callable[[str], None] = to_console(printf("%s"))
-        def _arrow6(arg: str) -> None:
+        def _arrow23(arg: str) -> None:
             clo(arg)
 
-        return _arrow6
+        return _arrow23
 
-    def _arrow8(__unit: None=None, args: Any=args) -> int:
+    def _arrow25(__unit: None=None, args: Any=args) -> int:
         return roll_dice()
 
-    result: int = roll_progressively(_arrow7(), _arrow8, True, 2147483647 // 10) or 0
-    def _arrow9(__unit: None=None, args: Any=args) -> str:
+    result: int = roll_progressively(_arrow24(), _arrow25, True, 2147483647 // 10) or 0
+    def _arrow26(__unit: None=None, args: Any=args) -> str:
         return ("main / result: " + str(result)) + ""
 
-    def _arrow10(__unit: None=None, args: Any=args) -> str:
+    def _arrow27(__unit: None=None, args: Any=args) -> str:
         return ""
 
-    trace(TraceLevel(1), _arrow9, _arrow10)
+    trace(TraceLevel(1), _arrow26, _arrow27)
     return 0
 
 
