094d430f5ebff3a5503dfa23490cc54098e8298e
프리마 <fc1943s@pm.me>
2025-01-27 09:31:37 -0300

    tests

--- lib/build.ps1
+++ lib/build.ps1
@@ -1,6 +1,9 @@
 param(
     $fast,
     $SkipNotebook,
+    $SkipRust,
+    $SkipTypeScript,
+    $SkipPython,
     $ScriptDir = $PSScriptRoot
 )
 $ScriptDir | Set-Location
@@ -29,73 +32,81 @@ $builderArgs = @("$projectName.fsx", $runtime, "--packages", "Fable.Core", "--mo
 
 $targetDir = GetTargetDir $projectName
 
-{ BuildFable $targetDir $projectName "rs" } | Invoke-Block
+if (!$SkipRust) {
+    { BuildFable $targetDir $projectName "rs" } | Invoke-Block
 
-$Path = "$targetDir/target/rs/$projectName.rs"
-if (!($Path | Test-Path)) {
-    $Path = "$targetDir/target/rs/polyglot/target/Builder/$projectName/$projectName.rs"
+    $Path = "$targetDir/target/rs/$projectName.rs"
+    if (!($Path | Test-Path)) {
+        $Path = "$targetDir/target/rs/polyglot/target/Builder/$projectName/$projectName.rs"
+    }
+    $Target = "$projectName.rs"
+    Write-Output "dice/lib/build.ps1 / Path: $Path / Target: $Target"
+    (Get-Content $Path) `
+        -replace "`"./lib", "`"../deps/polyglot/lib" `
+        -replace "`"../../../../../deps/spiral", "`"../deps/polyglot/deps/spiral" `
+        -replace "../../../lib", "../deps/polyglot/deps/spiral/lib" `
+        -replace "../../../../../../../../../../../../polyglot", "../deps/polyglot" `
+        -replace "../../../deps/polyglot", "../deps/polyglot" `
+        -replace ".fsx`"]", ".rs`"]" `
+        | FixRust `
+        | Set-Content $Target
 }
-$Target = "$projectName.rs"
-Write-Output "dice/lib/build.ps1 / Path: $Path / Target: $Target"
-(Get-Content $Path) `
-    -replace "`"./lib", "`"../deps/polyglot/lib" `
-    -replace "`"../../../../../deps/spiral", "`"../deps/polyglot/deps/spiral" `
-    -replace "../../../lib", "../deps/polyglot/deps/spiral/lib" `
-    -replace "../../../../../../../../../../../../polyglot", "../deps/polyglot" `
-    -replace "../../../deps/polyglot", "../deps/polyglot" `
-    -replace ".fsx`"]", ".rs`"]" `
-    | FixRust `
-    | Set-Content $Target
 
-{ BuildFable $targetDir $projectName "ts" } | Invoke-Block
+if (!$SkipTypeScript) {
+    { BuildFable $targetDir $projectName "ts" } | Invoke-Block
 
-$Path = "$targetDir/target/ts/$projectName.ts"
-if (!($Path | Test-Path)) {
-    { ls } | Invoke-Block -Location "$targetDir/target/ts"
-    $Path = "$targetDir/target/ts/polyglot/target/Builder/$projectName/$projectName.ts"
+    $Path = "$targetDir/target/ts/$projectName.ts"
+    if (!($Path | Test-Path)) {
+        { ls } | Invoke-Block -Location "$targetDir/target/ts"
+        $Path = "$targetDir/target/ts/polyglot/target/Builder/$projectName/$projectName.ts"
+    }
+    $Target = "$projectName.ts"
+    Write-Output "dice/lib/build.ps1 / Path: $Path / Target: $Target"
+    (Get-Content $Path) `
+        | FixTypeScript `
+        | FixTypeScriptExternal `
+        | Set-Content $Target
 }
-$Target = "$projectName.ts"
-Write-Output "dice/lib/build.ps1 / Path: $Path / Target: $Target"
-(Get-Content $Path) `
-    | FixTypeScript `
-    | FixTypeScriptExternal `
-    | Set-Content $Target
 
-{ BuildFable $targetDir $projectName "py" } | Invoke-Block
+if (!$SkipPython) {
+    { BuildFable $targetDir $projectName "py" } | Invoke-Block
 
-$Path = "$targetDir/target/py/$projectName.py"
-if (!($Path | Test-Path)) {
-    $Path = "$targetDir/target/py/polyglot/target/Builder/$projectName/$projectName.py"
+    $Path = "$targetDir/target/py/$projectName.py"
+    if (!($Path | Test-Path)) {
+        $Path = "$targetDir/target/py/polyglot/target/Builder/$projectName/$projectName.py"
+    }
+    $Target = "$projectName.py"
+    Write-Output "dice/lib/build.ps1 / Path: $Path / Target: $Target"
+    Copy-Item $Path $Target -Force
+
+    { BuildFable $targetDir $projectName "rs" "CONTRACT" } | Invoke-Block
 }
-$Target = "$projectName.py"
-Write-Output "dice/lib/build.ps1 / Path: $Path / Target: $Target"
-Copy-Item $Path $Target -Force
 
-{ BuildFable $targetDir $projectName "rs" "CONTRACT" } | Invoke-Block
+if (!$SkipRust) {
+    $Path = "$targetDir/target/rs/$projectName.rs"
+    if (!($Path | Test-Path)) {
+        $Path = "$targetDir/target/rs/polyglot/target/Builder/$projectName/$projectName.rs"
+    }
+    $Target = "$($projectName)_contract.rs"
+    Write-Output "dice/lib/build.ps1 / Path: $Path / Target: $Target"
+    (Get-Content $Path) `
+        -replace "`"../../../../../lib", "`"../deps/polyglot/deps/spiral/lib" `
+        -replace "`"./lib", "`"../deps/polyglot/lib" `
+        -replace "`"../../../../../deps/spiral", "`"../deps/polyglot/deps/spiral" `
+        -replace "../../../../../../../../../../../../polyglot", "../deps/polyglot" `
+        -replace "../../../deps/polyglot", "../deps/polyglot" `
+        -replace ".fsx`"]", ".rs`"]" `
+        -replace ".rs`"]", "_contract.rs`"]" `
+        -replace "use fable_library_rust::DateTime_::DateTime;", "type DateTime = ();" `
+        | FixRust `
+        | Set-Content $Target
 
-$Path = "$targetDir/target/rs/$projectName.rs"
-if (!($Path | Test-Path)) {
-    $Path = "$targetDir/target/rs/polyglot/target/Builder/$projectName/$projectName.rs"
-}
-$Target = "$($projectName)_contract.rs"
-Write-Output "dice/lib/build.ps1 / Path: $Path / Target: $Target"
-(Get-Content $Path) `
-    -replace "`"../../../../../lib", "`"../deps/polyglot/deps/spiral/lib" `
-    -replace "`"./lib", "`"../deps/polyglot/lib" `
-    -replace "`"../../../../../deps/spiral", "`"../deps/polyglot/deps/spiral" `
-    -replace "../../../../../../../../../../../../polyglot", "../deps/polyglot" `
-    -replace "../../../deps/polyglot", "../deps/polyglot" `
-    -replace ".fsx`"]", ".rs`"]" `
-    -replace ".rs`"]", "_contract.rs`"]" `
-    -replace "use fable_library_rust::DateTime_::DateTime;", "type DateTime = ();" `
-    | FixRust `
-    | Set-Content $Target
+    cargo fmt --
+    { cargo fmt -- } | Invoke-Block -Location contract
 
-cargo fmt --
-{ cargo fmt -- } | Invoke-Block -Location contract
-
-if (!$fast) {
-    { cargo run --release } | Invoke-Block
+    if (!$fast) {
+        { cargo run --release } | Invoke-Block
+    }
 }
 
 if ($env:CI) {
